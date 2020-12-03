ac9e1a3dd5aa1fcdc0573480a1ffbfa7ad7ce5d6
Robert <rbbenett0614@gmail.com>
2020-12-03 20:39:38 +0000

    Third Commit:

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,6 +4,9 @@ const fetchBreedDescription = function(breedName, callback) {
   const website = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   request(website,  (error, response, body) => {
     const data = JSON.parse(body);
+    if(data.length === 0) {
+      return callback('Breed not found')
+    }
     const desc = data[0]['description'];
     if (error) {
       return callback(error);
